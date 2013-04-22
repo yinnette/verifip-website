3e116a797da48933f775a2f22b65889a1331ce4d
Nick <nick@nick.(none)>
2013-04-21 22:50:53 -0400

    add s3utils

---
+++ s3utils/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='s3utils',
+    version='0.0.1',
+    author='Aleksandr Vladimirskiy',
+    author_email='aleksandr@butchershop.co',
+    packages=['s3utils',],
+    scripts=[],
+    url='',
+    license='',
+    description='S3 Utils',
+    # long_description=open('').read(),
+    install_requires=[
+    ],
+)
